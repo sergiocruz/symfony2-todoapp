19bdaceddcd5b1a2d7fbd5ba1ca5b67ca37c1427
Sergio Cruz <sergio@cruzcorp.net>
2014-02-04 22:39:43 -0500

    got angular working

--- web/bundles/cruz/js/todo.js
+++ web/bundles/cruz/js/todo.js
@@ -1,5 +1,42 @@
-var todo = angular.module('todoapp');
+var module = angular.module('todoApp', [], function () {
 
-todo.controller('TodoController', ['$scope', function($scope) {
-	$scope.todos = ['test', 'test2'];
-}]);
+});
+
+module.controller('todoCrl', function ($scope) {
+
+    $scope.todos = [
+        {
+            text: 'Test',
+            completed: true
+        },
+
+        {
+            text: 'Test 2',
+            completed: true
+        },
+
+        {
+            text: 'Test 3',
+            completed: false
+        }
+    ];
+
+    $scope.todoText = '';
+    $scope.addTodo = function() {
+
+        // Does nothing if text is empty
+        if (!$scope.todoText)
+        {
+            return;
+        }
+
+        // Adds todo to array
+        $scope.todos.push({
+            text: $scope.todoText,
+            completed: false
+        });
+
+        // Resets todo tetx
+        $scope.todoText = '';
+    };
+});
